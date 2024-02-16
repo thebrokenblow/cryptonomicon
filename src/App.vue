b563c1f7bced557a47b633fc4f6d8a8eb98ff22c
Artem Krasov <artkrasov@gmail.com>
2024-02-17 00:08:04 +0300

    Added filtering, pagination

--- src/App.vue
+++ src/App.vue
@@ -42,10 +42,27 @@
       </section>
 
       <template v-if="tickers.length">
+        <div>
+          Фильтр: <input v-model="filter" type="text" />
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="page > 1"
+            @click="page = page - 1"
+          >
+            Назад
+          </button>
+          <button
+            v-if="hasNextPage"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            @click="page = page + 1"
+          >
+            Вперёд
+          </button>
+        </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -129,10 +146,24 @@ export default {
       ticker: '',
       tickers: [],
       sel: null,
-      graph: []
+      graph: [],
+      page: 1,
+      filter: '',
+      hasNextPage: true
     }
   },
+
   created() {
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries())
+
+    if (windowData.filter) {
+      this.filter = windowData.filter
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page
+    }
+
     const tikersData = localStorage.getItem('cryptonomicon-list')
 
     if (tikersData) {
@@ -142,6 +173,16 @@ export default {
   },
 
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * 6
+      const end = this.page * 6
+
+      const filteredTickers = this.tickers.filter((ticker) => ticker.name.includes(this.filter))
+      this.hasNextPage = filteredTickers.length > end
+
+      return filteredTickers.slice(start, end)
+    },
+
     subscribeToUpdate(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -157,6 +198,7 @@ export default {
         }
       }, 5000)
     },
+
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -167,6 +209,7 @@ export default {
       localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
       this.subscribeToUpdate(currentTicker.name)
       this.ticker = ''
+      this.filter = ''
     },
 
     select(ticker) {
@@ -183,6 +226,23 @@ export default {
       const maxValue = Math.max(...this.graph)
       const minValue = Math.min(...this.graph)
       return this.graph.map((price) => 5 + ((price - minValue) * 95) / (maxValue - minValue))
+    }
+  },
+  watch: {
+    filter() {
+      this.page = 1
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      )
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      )
     }
   }
 }
