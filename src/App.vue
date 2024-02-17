9c8ee13acb10fca71f1476c31da8b0032a8ed959
Artem Krasov <artkrasov@gmail.com>
2024-02-17 16:57:20 +0300

    Refactored code

--- src/App.vue
+++ src/App.vue
@@ -42,8 +42,8 @@
       </section>
 
       <template v-if="tickers.length">
+        <hr class="w-full border-t border-gray-600 my-4" />
         <div>
-          Фильтр: <input v-model="filter" type="text" />
           <button
             class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             v-if="page > 1"
@@ -52,12 +52,13 @@
             Назад
           </button>
           <button
-            v-if="hasNextPage"
             class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             @click="page = page + 1"
+            v-if="hasNextPage"
           >
-            Вперёд
+            Вперед
           </button>
+          <div>Фильтр: <input v-model="filter" @input="page = 1" /></div>
         </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
@@ -144,11 +145,11 @@ export default {
   data() {
     return {
       ticker: '',
+      filter: '',
       tickers: [],
-      sel: null,
       graph: [],
       page: 1,
-      filter: '',
+      sel: null,
       hasNextPage: true
     }
   },
@@ -164,11 +165,13 @@ export default {
       this.page = windowData.page
     }
 
-    const tikersData = localStorage.getItem('cryptonomicon-list')
+    const tickersData = localStorage.getItem('cryptonomicon-list')
 
-    if (tikersData) {
-      this.tickers = JSON.parse(tikersData)
-      this.tickers.forEach((ticker) => this.subscribeToUpdate(ticker.name))
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData)
+      this.tickers.forEach((ticker) => {
+        this.subscribeToUpdates(ticker.name)
+      })
     }
   },
 
@@ -178,18 +181,20 @@ export default {
       const end = this.page * 6
 
       const filteredTickers = this.tickers.filter((ticker) => ticker.name.includes(this.filter))
+
       this.hasNextPage = filteredTickers.length > end
 
       return filteredTickers.slice(start, end)
     },
 
-    subscribeToUpdate(tickerName) {
+    subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
           `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=ce3fd966e7a1d10d65f907b20bf000552158fd3ed1bd614110baa0ac6cb57a7e`
         )
         const data = await f.json()
 
+        // currentTicker.price =  data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
         this.tickers.find((t) => t.name === tickerName).price =
           data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2)
 
@@ -197,6 +202,7 @@ export default {
           this.graph.push(data.USD)
         }
       }, 5000)
+      this.ticker = ''
     },
 
     add() {
@@ -206,10 +212,10 @@ export default {
       }
 
       this.tickers.push(currentTicker)
-      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
-      this.subscribeToUpdate(currentTicker.name)
-      this.ticker = ''
       this.filter = ''
+
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
+      this.subscribeToUpdates(currentTicker.name)
     },
 
     select(ticker) {
@@ -222,12 +228,12 @@ export default {
     },
 
     normalizeGraph() {
-      console.log('Привет')
       const maxValue = Math.max(...this.graph)
       const minValue = Math.min(...this.graph)
       return this.graph.map((price) => 5 + ((price - minValue) * 95) / (maxValue - minValue))
     }
   },
+
   watch: {
     filter() {
       this.page = 1
@@ -247,5 +253,3 @@ export default {
   }
 }
 </script>
-
-<style src="./app.css"></style>
