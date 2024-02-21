6136e09b0506631bec382f4f26e6b23a5219975a
Artem Krasov <artkrasov@gmail.com>
2024-02-21 15:31:42 +0300

    Вынес график в компоненты

--- src/App.vue
+++ src/App.vue
@@ -62,42 +62,7 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div class="flex items-end border-gray-600 border-b border-l h-64" ref="graph">
-          <div
-            v-for="(bar, idx) in normalizedGraph"
-            :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button @click="selectedTicker = null" type="button" class="absolute top-0 right-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <graph-ticker :selectedTicker="selectedTicker" :price="priceSelectTicker" @remove-select-ticker="selectedTicker = null"/>
     </div>
   </div>
 </template>
@@ -105,23 +70,24 @@
 <script>
 import { subscribeToTicker, unsubscribeFromTicker, getAllTickers } from './api'
 import AddTicker from './components/AddTicker.vue'
+import GraphTicker from './components/GraphTicker.vue'
 
 export default {
   name: 'App',
 
   components: {
-    AddTicker
+    AddTicker,
+    GraphTicker
   },
 
   data() {
     return {
       filter: '',
-      graph: [],
       tickers: [],
       page: 1,
-      maxGraphElements: 1,
       isTickerEntered: false,
-      selectedTicker: null
+      selectedTicker: null,
+      priceSelectTicker: 0
     }
   },
 
@@ -137,7 +103,6 @@ export default {
     })
 
     const tickersData = localStorage.getItem('cryptonomicon-list')
-
     if (tickersData) {
       this.tickers = JSON.parse(tickersData)
       this.tickers.forEach((ticker) => {
@@ -146,14 +111,6 @@ export default {
     }
 
     getAllTickers()
-  },
-
-  mounted() {
-    window.addEventListener('resize', this.calculateMaxGraphElements)
-  },
-
-  beforeUnmount() {
-    window.removeEventListener('resize', this.calculateMaxGraphElements)
   },
 
   computed: {
@@ -177,17 +134,6 @@ export default {
       return this.filteredTickers.length > this.endIndex
     },
 
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph)
-      const minValue = Math.min(...this.graph)
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50)
-      }
-
-      return this.graph.map((price) => 5 + ((price - minValue) * 95) / (maxValue - minValue))
-    },
-
     pageStateOptions() {
       return {
         filter: this.filter,
@@ -197,28 +143,6 @@ export default {
   },
 
   methods: {
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return
-      }
-
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38
-    },
-
-    updateTicker(tickerName, price) {
-      this.tickers
-        .filter((t) => t.name === tickerName)
-        .forEach((t) => {
-          if (t === this.selectedTicker) {
-            this.graph.push(price)
-            while (this.graph.length > this.maxGraphElements) {
-              this.graph.shift()
-            }
-          }
-          t.price = price
-        })
-    },
-
     formatPrice(price) {
       if (price === '-') {
         return price
@@ -226,15 +150,18 @@ export default {
       return price > 1 ? price.toFixed(2) : price.toPrecision(2)
     },
 
-    add(ticker) {
-      const nameMatch = this.tickers.filter(
-        (currentTicker) => currentTicker.name.toLowerCase() === ticker.toLowerCase()
-      )
-      this.isTickerEntered = nameMatch.length != 0
-      if (this.isTickerEntered) {
-        return
-      }
+    updateTicker(tickerName, price) {
+      this.tickers
+        .filter((ticker) => ticker.name === tickerName)
+        .forEach((ticker) => {
+          if (this.selectedTicker != null && this.selectedTicker.name === ticker.name) {
+            this.priceSelectTicker = price
+          }
+          ticker.price = price
+        })
+    },
 
+    add(ticker) {
       const currentTicker = {
         name: ticker,
         price: '-'
@@ -242,6 +169,7 @@ export default {
 
       this.tickers = [...this.tickers, currentTicker]
       this.filter = ''
+
       subscribeToTicker(currentTicker.name, (newPrice) =>
         this.updateTicker(currentTicker.name, newPrice)
       )
@@ -261,13 +189,7 @@ export default {
   },
 
   watch: {
-    selectedTicker() {
-      this.graph = []
-
-      this.$nextTick().then(this.calculateMaxGraphElements)
-    },
-
-    tickers(newValue, oldValue) {
+    tickers() {
       localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers))
     },
 
